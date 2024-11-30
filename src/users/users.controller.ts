4b2f861d16fcc9826ba152faadc721b91a88e9ea
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-12-01 00:57:34 +0530

    added controller routes for  user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    /*
+    GET /users
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+    */
+
+    @Get() 
+    findAll(@Query('role') role ?: 'INTERN' | 'ENGINEER' | 'USER' ){
+        return [{role}]
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+      return { id }
+    }
+
+    @Post()
+    create(@Body() users: {}) {
+        return users
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() userUpdate: {}){
+        return { id, ...userUpdate }
+    }
+
+    @Delete(':id')
+    deleteOne(@Param('id') id: string){
+        return { id }
+    }
+
+}
